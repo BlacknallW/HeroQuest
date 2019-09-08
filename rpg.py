3a5c96aeed01af9bbff7861e3d97f3dc4a80861a
William Blacknall <williamblacknall@gmail.com>
2019-09-07 22:27:58 -0400

    Added stats to characters and editted power formula

--- rpg.py
+++ rpg.py
@@ -1,10 +1,11 @@
 import random
 
 class Character:
-    def __init__(self, name, power, health):
+    def __init__(self, name, health, stats, power):
         self.name = name
-        self.power = power
         self.health = health
+        self.stats = stats
+        self.power = power + (stats["Strength"] * .5)
 
     def attack(self, power, enemy):
         if enemy == medic:
@@ -34,27 +35,51 @@ class Character:
 class Hero(Character):
     def attack(self, power, enemy):
         double_damage = random.random()
+
         if double_damage <= .20:
-            critical_hit = power * 2
-            enemy.health -= critical_hit
+            self.power = power * 2
+            enemy.health -= self.power           
             print("CRITICAL HIT!")
-        else:
+        if double_damage > .20:
             enemy.health -= power
+
+        if enemy == medic:
+            recover_health = random.random()
+            if recover_health <= .20:
+                enemy.health -= power - 2
+                print("The Medic injects herself with a syringe, which is definitely not usually a good idea, and recovered 2 health!")
+
+        if enemy == shadow:
+            null_damage = random.random()
+            if null_damage <= .10:
+                enemy.health -= power
+                print(f"{shadow.name} took no damage from that attack!")
+            else:
+                enemy.health -= power
 
 class Berserker(Character):
     def attack(self, power, enemy):
         if berserker.health < 15:
             print("Sweet lord the Berserker has gone nuts and is doing double damage!")
             enemy.health -= power * 2
+            self.power = power * 2
         else:
             enemy.health -= power
 
-hero = Hero("Maximo", 5, 100)
-goblin = Character("Goblin", 2, 20)
-medic = Character("Medic", 3, 25)
-shadow = Character("Shadow", 10, 1)
-zombie = Character("Zombie", 1, 10)
-berserker = Berserker("Berserker", 10, 30)
+Humans = {"Strength" : 7, "Agility" : 7, "Constitution" : 7}
+Goblins = {"Strength" : 3, "Agility" : 7, "Constitution": 5 }
+Demons = {"Strength" : 5, "Agility" : 5, "Constitution" : 0}
+Centaurs = {"Strength" : 8, "Agility" : 8, "Constitution" : 5}
+Undead = {"Strength" : 2, "Agility" : 5, "Constitution" : 0}
+Amorphous = {"Strength" : 5, "Agility" : 10, "Constitution" : 5}
+Ogre = {"Strength" : 10, "Agility" : 3, "Constitution" : 8}
+
+hero = Hero("Maximo", 100, Humans, 5)
+goblin = Character("Goblin", 20, Goblins, 2)
+medic = Character("Medic", 25, Humans, 3)
+shadow = Character("Shadow", 1, Demons, 10)
+zombie = Character("Zombie", 10, Undead, 1)
+berserker = Berserker("Berserker", 30, Humans, 10)
 
 def main():
     print("Welcome to the world of...Frank. Yeah, just Frank. He's some guy. I guess he owns the world or something? Wild.")
@@ -82,13 +107,16 @@ def main():
                 if goblin.alive(goblin.health):
                     print("The goblin has been murdered. His only crime was being a goblin.")
             
-            if user_input == "2":
+            elif user_input == "2":
                 print(f"You stare at the goblin. Menacingly. You even throw up a gang sign or two. He freaks the fudge out and attacks you out of sheer terror.")
             
-            if user_input == "3":
+            elif user_input == "3":
                 print(f"Seriously? You're just going to run away? Coward. COWAAAAAAAAAAAAAAAAARD!!!")
                 break
             
+            else:
+                print("Hey bud. That wasn't one of the options you were given. Learn the rules!")
+
             if goblin.health > 0:
                 goblin.attack(goblin.power, hero)
                 print(f"The goblin does {goblin.power} damage to you!")
@@ -115,16 +143,19 @@ def main():
 
             if user_input == "1":
                 hero.attack(hero.power, berserker)
-                print(f"You've dealt {hero.power} to the {berserker.name}.")
+                print(f"You've dealt {hero.power} damage to the {berserker.name}.")
                 if berserker.alive(berserker.health):
                     print("The berserker has been slain!")
-            if user_input == "2":
+            elif user_input == "2":
                 print("You insult the Berserker's mother and start stomping on his flowers while, for some reason, throwing up gang signs. Unprovoked, the Berserker attacks!")
 
-            if user_input == "3":
+            elif user_input == "3":
                 print("Oh, oh, what?! You murder a goblin in cold blood without a problem but now all of a sudden you have a problem -DEFENDING YOURSELF- against a Berserker? Seriously? What are you, racist against goblins? I don't have time for bigots. Get out of here.")
                 break
             
+            else:
+                print("Oh boy. That's not one of the options you were given and yet you did it anyway. So uh...don't do that.")
+
             if berserker.health > 0:
                 berserker.attack(berserker.power, hero)
                 print(f"The {berserker.name} deals {berserker.power} damage to you. ")
@@ -158,13 +189,16 @@ def main():
                 print(f"You've dealt {hero.power} damage to the {medic.name}.")
                 if medic.alive(medic.health):
                     print("Oh wow you killed her.")
-            if user_input == "2":
+            elif user_input == "2":
                 print("Your eyes roll to the back of your head, you start foaming at the mouth and making demonic screeches while walking like a crab and snapping your imaginary claws. The Medic tries to exorcise you, horrified, which is interesting because medics don't typically do exorcism.")
 
-            if user_input == "3":
+            elif user_input == "3":
                 print("You try to flee like a small, worthless child but the medic tranquilizes you with...a tranquilizer...gun? What time period is this game set in? Let's say it was a dart. Anyway, she hits you with it and then you go to jail or something. Lawyer up, my man.")
                 break
             
+            else:
+                print("Nah don't do that. Press 1, 2, or 3.")
+
             if medic.health > 0:
                 medic.attack(medic.power, hero)
                 print(f"The {medic.name} deals {medic.power} damage to you.")
